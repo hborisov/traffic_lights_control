eee6f78e729a5369922dd05c945c646ae60f22cd
Hristo Borisov <ico.borisov@gmail.com>
2015-09-15 12:03:39 +0300

    Initial Commit

---
+++ UI_example/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  UI_example
+//
+//  Created by Hristo Borisov on 9/15/15.
+//  Copyright (c) 2015 Hristo Borisov. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
